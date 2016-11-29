a40ab2ff79c56ae314760622120e81b3a5e1401a
Maciej Kozera <maciek.kozera@gmail.com>
2016-11-29 22:36:10 +0100

    minor change to unit tests

--- FrontEnd/src/__tests__/test.jsx
+++ FrontEnd/src/__tests__/test.jsx
@@ -1,10 +1,11 @@
 import configureMockStore from 'redux-mock-store';
 import expect from 'expect';
-import nock from 'nock';
 import thunk from 'redux-thunk'
 import * as ACTIONCREATORS from  './../actionCreators/ActionCreators';
 import * as ACTIONS from './../actionCreators/Actions';
 import { ApiClient } from './../api/ApiClient';
+import 'isomorphic-fetch';
+import nock from 'nock';
 
 const middlewares = [ thunk ]
 const mockStore = configureMockStore(middlewares)
@@ -24,7 +25,10 @@ describe('action creators', () => {
 
 describe('API Client', () => {
 
-  let serverAddress = 'http://testserver/';
+  let serverAddress = 'http://testserver';
+  let personsEndpoint = '/persons';
+
+  let peronsEndpointUrl = serverAddress + personsEndpoint;
 
   afterEach(() => {
       nock.cleanAll()
@@ -32,10 +36,10 @@ describe('API Client', () => {
 
   it('should resolve promise if call to server made succesfully', () => {
     nock(serverAddress)
-      .get('/persons')
-      .reply(200, { body: [{id: '1', forename: 'test_forename', surname: 'test_surname', dateOfBirth: '01-01-1970'}] });
+      .get(personsEndpoint)
+      .reply(200, [{id: '1', forename: 'test_forename', surname: 'test_surname', dateOfBirth: '01-01-1970'}]);
 
-    let apiClient = new ApiClient(serverAddress);
+    let apiClient = new ApiClient(peronsEndpointUrl);
 
     return apiClient.fetchPersons().then((persons) => {
       expect(persons.length).toEqual(1);
@@ -44,10 +48,10 @@ describe('API Client', () => {
 
   it('should reject promise if call to server failed', () => {
     nock(serverAddress)
-      .get('/persons')
+      .get(personsEndpoint)
       .reply(404);
 
-    let apiClient = new ApiClient(serverAddress);
+    let apiClient = new ApiClient(peronsEndpointUrl);
 
     return apiClient.fetchPersons().catch((ex) => {
         expect(ex).toExist();
