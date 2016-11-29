a55c1fb8559e6e39dc1fa9c90fea49abcab9b589
mkozera <m.kozera@kainos.com>
2016-11-29 09:31:23 +0100

    more unit tests added

--- FrontEnd/src/__tests__/test.jsx
+++ FrontEnd/src/__tests__/test.jsx
@@ -1,8 +1,10 @@
 import configureMockStore from 'redux-mock-store';
 import expect from 'expect';
+import nock from 'nock';
 import thunk from 'redux-thunk'
 import * as ACTIONCREATORS from  './../actionCreators/ActionCreators';
 import * as ACTIONS from './../actionCreators/Actions';
+import { ApiClient } from './../api/ApiClient';
 
 const middlewares = [ thunk ]
 const mockStore = configureMockStore(middlewares)
@@ -17,5 +19,38 @@ describe('action creators', () => {
 
     store.dispatch(ACTIONCREATORS.PERSONS.fetchAllPersonsInProgress());
     expect(store.getActions()).toEqual(expectedActions);
+  });
+});
+
+describe('API Client', () => {
+
+  let serverAddress = 'http://testserver/';
+
+  afterEach(() => {
+      nock.cleanAll()
+  });
+
+  it('should resolve promise if call to server made succesfully', () => {
+    nock(serverAddress)
+      .get('/persons')
+      .reply(200, { body: [{id: '1', forename: 'test_forename', surname: 'test_surname', dateOfBirth: '01-01-1970'}] });
+
+    let apiClient = new ApiClient(serverAddress);
+
+    return apiClient.fetchPersons().then((persons) => {
+      expect(persons.length).toEqual(1);
+    });
+  });
+
+  it('should reject promise if call to server failed', () => {
+    nock(serverAddress)
+      .get('/persons')
+      .reply(404);
+
+    let apiClient = new ApiClient(serverAddress);
+
+    return apiClient.fetchPersons().catch((ex) => {
+        expect(ex).toExist();
+    });
   });
 });
