5b2f5aec94c02d3443c01668fd273e23ff4cebca
Maciej Kozera <maciek.kozera@gmail.com>
2016-12-12 19:03:36 +0100

    added reseting form after changing route

--- FrontEnd/src/router/routeConfig.jsx
+++ FrontEnd/src/router/routeConfig.jsx
@@ -3,11 +3,18 @@ import { Route, IndexRoute } from 'react-router';
 import Application from './../containers/Application';
 import Home from './../containers/Home';
 import Editor from './../containers/Editor';
+import { reset } from 'redux-form';
+
+const resetForm = (store, dispatch) => {
+  return (nextState, replace) => {
+      dispatch(resetForm('newPost'));
+  }
+}
 
 export const routeConfig = (
     <Route path="/" component={Application}>
       <IndexRoute component={Home}/>
-      <Route path="editor" component={Editor} />
+      <Route path="editor" component={Editor} onEnter={resetForm} />
       <Route path="editor/:id" component={Editor} />
     </Route>
 )
