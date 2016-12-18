432a9a3bb676e05e01c8ac46d3af57e057877752
Maciej Kozera <maciek.kozera@gmail.com>
2016-12-18 09:59:24 +0100

    inital work for including Draft.js rich editor into post editor component

--- FrontEnd/src/components/posts/PostEditor.jsx
+++ FrontEnd/src/components/posts/PostEditor.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import * as ACTION_CREATORS from './../../actionCreators/ActionCreators'
+import {Editor, EditorState, RichUtils} from 'draft-js';
 
 @connect(state => ({ initialValues: state.posts.currentPost }))
 @reduxForm({form: 'newPost'})
-export default class Editor extends React.Component{
+export default class PostEditor extends React.Component{
     constructor(props) {
         super(props);
+        this.state = {editorState: EditorState.createEmpty()};
+        this.onChange = (editorState) => this.setState({editorState});
     }
     submitForm(data) {
         if (data.id) {
@@ -16,11 +19,20 @@ export default class Editor extends React.Component{
             this.props.dispatch(ACTION_CREATORS.POSTS.addNewPost(data));
         } 
     }
+    _onBoldClick() {
+        this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+    }
     render() {
-        const { props: { handleSubmit }, submitForm } = this;
+        {/*const { props: { handleSubmit }, submitForm } = this;*/}
 
         return (
             <div>
+                <div>
+                    <button onClick={this._onBoldClick.bind(this)}>BOLD</button>
+                </div>
+                <Editor editorState={this.state.editorState} onChange={this.onChange} />
+            </div>
+            /*<div>
                 <form onSubmit={handleSubmit(submitForm.bind(this))}>
                     <div className="form-group">
                         <label htmlFor="title">Title</label>
@@ -34,7 +46,7 @@ export default class Editor extends React.Component{
                         <button type="submit" className="btn btn-primary">Submit</button>
                     </div>
                 </form>
-            </div>
+            </div>*/
         );
     }
 }
