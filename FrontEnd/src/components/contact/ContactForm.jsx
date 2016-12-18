a34b82a06dbaea224ab65f0bc3af292345ba8077
Maciej Kozera <maciek.kozera@gmail.com>
2016-12-18 09:57:07 +0100

    added contact form component

---
+++ FrontEnd/src/components/contact/ContactForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form';
+import * as ACTION_CREATORS from './../../actionCreators/ActionCreators'
+
+@connect()
+export default class ContactForm extends React.Component {
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
