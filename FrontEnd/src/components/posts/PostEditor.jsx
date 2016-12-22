acc3a07938159a65ed46cec98a8728107a92a764
Maciej Kozera <maciek.kozera@gmail.com>
2016-12-22 21:27:47 +0100

    more implementation of post editor, minor refactor around existing code

--- FrontEnd/src/components/posts/PostEditor.jsx
+++ FrontEnd/src/components/posts/PostEditor.jsx
@@ -1,78 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Field, reduxForm } from 'redux-form';
 import * as ACTION_CREATORS from './../../actionCreators/ActionCreators'
 import {Editor, EditorState, RichUtils} from 'draft-js';
 
+import BlockStyleControls from './editor/blockStyleControls';
+import InlineStyleControls from './editor/inlineStyleControls';
 
-const BLOCK_TYPES = [
-  {label: 'H1', style: 'header-one'},
-  {label: 'H2', style: 'header-two'},
-  {label: 'H3', style: 'header-three'},
-  {label: 'H4', style: 'header-four'},
-  {label: 'H5', style: 'header-five'},
-  {label: 'H6', style: 'header-six'},
-  {label: 'Blockquote', style: 'blockquote'},
-  {label: 'UL', style: 'unordered-list-item'},
-  {label: 'OL', style: 'ordered-list-item'},
-  {label: 'Code Block', style: 'code-block'},
-];
-
-class StyleButton extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    onToggle(e) {
-        e.preventDefault();
-        this.props.onToggle(this.props.style);
-    }
-    render() {
-        let classes = "btn btn-secondary";
-        if(this.props.active) {
-            classes += " used-style"
-        }
-
-        return (
-            <span className={classes} onMouseDown={this.onToggle.bind(this)}>
-                {this.props.text}
-            </span>
-        )
-    }
-}
-
-class BlockStyleControls extends React.Component {
-    toggleBlockStyle(styleName) {
-        this.props.onToggle(styleName);
-    }
-    render() {
-        const selection = this.props.editorState.getSelection();
-        const blockType = this.props.editorState
-            .getCurrentContent()
-            .getBlockForKey(selection.getStartKey())
-            .getType();
-        const toggleBlockStyle = this.toggleBlockStyle.bind(this);
-        return (
-            <div>
-                {BLOCK_TYPES.map((type) => {
-                    return(
-                        <StyleButton onToggle={() => { toggleBlockStyle(type.style); }} key={type.label} text={type.label} style={type.style} active={type.style === blockType} />
-                    )   
-                })}
-            </div>
-        )
-    }
-}
-
-@connect(state => ({ initialValues: state.posts.currentPost }))
-@reduxForm({form: 'newPost'})
+@connect()
 export default class PostEditor extends React.Component{
     constructor(props) {
         super(props);
         this.state = {editorState: EditorState.createEmpty()};
         this.onChange = (editorState) => this.setState({editorState});
         this.focus = () => this.refs.editor.focus();
+        this.toggleBlockType = this.toggleBlockType.bind(this);
+        this.toggleInlineStyle = this.toggleInlineStyle.bind(this);
     }
-    _toggleBlockType(blockType) {
+    toggleBlockType(blockType) {
         this.onChange(
             RichUtils.toggleBlockType(
                 this.state.editorState,
@@ -80,23 +24,22 @@ export default class PostEditor extends React.Component{
             )
         );
     }
-    submitForm(data) {
-        if (data.id) {
-            this.props.dispatch(ACTION_CREATORS.POSTS.updatePost(data));
-        } else {
-            this.props.dispatch(ACTION_CREATORS.POSTS.addNewPost(data));
-        } 
-    }
-    _onBoldClick() {
-        this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+    toggleInlineStyle(inlineStyle) {
+        this.onChange(
+            RichUtils.toggleInlineStyle(
+                this.state.editorState,
+                inlineStyle
+            )
+        );
     }
     render() {
         return (
             <div>
                 <div>
-                    <BlockStyleControls onToggle={this._toggleBlockType.bind(this)} editorState={this.state.editorState} />
+                    <BlockStyleControls onToggle={this.toggleBlockType} editorState={this.state.editorState} />
+                    <InlineStyleControls onToggle={this.toggleInlineStyle} editorState={this.state.editorState} />
                 </div>
-                <div onClick={this.focus} style={{minHeight: 300, borderColor: 'black', borderWidth: 1, borderStyle: 'solid', padding: 5}}>
+                <div onClick={this.focus} className="post-editor-wrapper">
                     <Editor editorState={this.state.editorState} onChange={this.onChange} ref="editor"/>
                 </div>
             </div>
--- FrontEnd/src/router/routeConfig.jsx
+++ FrontEnd/src/router/routeConfig.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import Application from './../components/layout/Application';
 import Home from './../components/layout/Home';
-import PostEditor from './../components/posts/PostEditor';
+import PostEditor from './../components/posts/postEditor';
 import { reset } from 'redux-form';
 
 const resetForm = (store, dispatch) => {
